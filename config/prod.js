97e7db953a43526c848a66c45da62ac06de0e196
Isabel Mill <isabelmavd2@gmail.com>
2022-07-19 00:17:16 +0300

    env file

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb+srv://Isabel:Isabel77@cluster0.ffepb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  // dbURL: 'mongodb+srv://Isabel:Isabel77@cluster0.ffepb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  dbURL: process.env.MONGODB_URL,
 }
